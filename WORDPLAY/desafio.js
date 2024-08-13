18afae959e6ffc0595bb7fe2bde6c5112f42cabf
Thalisson Moura <105744721+Thalis78@users.noreply.github.com>
2024-08-12 23:25:11 -0300

    update

--- WORDPLAY/desafio.js
+++ WORDPLAY/desafio.js
@@ -1,16 +1,41 @@
 import { question } from "readline-sync";
-import {readFileSync} from "fs"
-import { listinha_arquivo,listinha_com_vinte_letras,listinha_sem_letra_e,escolha_menu } from "./utilsStrings.js";
+import { listinha_arquivo,listinha_com_vinte_letras,listinha_sem_letra_e,opcao_menu,porcentagem,mostrar_lista} from "./utilsStrings.js";
 
 
 
 function main(){
-    
     const lista_arquivo = listinha_arquivo();
     const lista_palavra_mais_vinte = listinha_com_vinte_letras(lista_arquivo);
     const lista_palavra_sem_E = listinha_sem_letra_e(lista_arquivo);
-    
-    escolha_menu(lista_arquivo,lista_palavra_mais_vinte,lista_palavra_sem_E);
-    
+    let menu = Number(question(opcao_menu()))
+    let lista_com_n_e_letras = 0;
+    switch(menu){
+        case 1:
+            mostrar_lista(lista_arquivo)
+            console.log(`
+                CARREGAMENTO DE ARQUIVO FEITO COM SUCESSO
+                QUANTIDADE DE PALAVRAS : ${lista_arquivo.length.toFixed(2)} %`);
+            break;
+        case 2:
+            mostrar_lista(lista_palavra_mais_vinte)
+            console.log(`
+                QUANTIDADE DE PALAVRAS : ${lista_palavra_mais_vinte.length}
+                PORCENTAGEM            : ${porcentagem(lista_arquivo.length,lista_palavra_mais_vinte.length).toFixed(2)} %`);
+
+           break;
+        case 3:
+            mostrar_lista(lista_palavra_sem_E)
+                console.log(`
+                QUANTIDADE DE PALAVRAS SEM A LETRA E: ${lista_palavra_sem_E.length}
+                PORCENTAGEM                         : ${porcentagem(lista_arquivo.length,lista_palavra_sem_E.length).toFixed(2)} %`);
+            break;
+        case 4: 
+            lista_com_n_e_letras = listinha_letra_n_mais_letras(lista_arquivo);
+            mostrar_lista(lista_com_n_e_letras)
+            console.log(`
+                QUANTIDADE DE PALAVRAS SEM A LETRA E: ${lista_com_n_e_letras.length}
+                PORCENTAGEM                         : ${porcentagem(lista_arquivo.length,lista_com_n_e_letras.length).toFixed(2)} %`);
+            break;
+    }
 }
 main()
--- WORDPLAY/utilsStrings.js
+++ WORDPLAY/utilsStrings.js
@@ -11,39 +11,7 @@ export function listinha_arquivo(){
     }
     return array
 }
-export function escolha_menu(lista_arquivo,lista_palavra_mais_vinte,lista_palavra_sem_E){
-    let menu = Number(question(opcao_menu()))
-    let lista_com_n_e_letras = 0;
-    switch(menu){
-        case 1:
-            mostrar_lista(lista_arquivo)
-            console.log(`
-                CARREGAMENTO DE ARQUIVO FEITO COM SUCESSO
-                QUANTIDADE DE PALAVRAS : ${lista_arquivo.length.toFixed(2)} %`);
-            break;
-        case 2:
-            mostrar_lista(lista_palavra_mais_vinte)
-            console.log(`
-                QUANTIDADE DE PALAVRAS : ${lista_palavra_mais_vinte.length}
-                PORCENTAGEM            : ${porcentagem(lista_arquivo.length,lista_palavra_mais_vinte.length).toFixed(2)} %`);
-
-           break;
-        case 3:
-            mostrar_lista(lista_palavra_sem_E)
-                console.log(`
-                QUANTIDADE DE PALAVRAS SEM A LETRA E: ${lista_palavra_sem_E.length}
-                PORCENTAGEM                         : ${porcentagem(lista_arquivo.length,lista_palavra_sem_E.length).toFixed(2)} %`);
-            break;
-        case 4: 
-            lista_com_n_e_letras = listinha_letra_n_mais_letras(lista_arquivo);
-            mostrar_lista(lista_com_n_e_letras)
-            console.log(`
-                QUANTIDADE DE PALAVRAS SEM A LETRA E: ${lista_com_n_e_letras.length}
-                PORCENTAGEM                         : ${porcentagem(lista_arquivo.length,lista_com_n_e_letras.length).toFixed(2)} %`);
-            break;
-    }
-}
-function opcao_menu(){
+export function opcao_menu(){
     return `
      1 - LOAD FILE
      2 - PALAVRAS C/ 20+ LETRAS
@@ -78,7 +46,7 @@ export function listinha_sem_letra_e(listinhaComPalavra){
 
 
 }
-function listinha_letra_n_mais_letras(listinhaComPalavra){
+export function listinha_letra_n_mais_letras(listinhaComPalavra){
     let array = []
     let quantidadeDeLetras = Number(question("INFORME A QUANTIDADE DE LETRAS: "))
     for(let i in listinhaComPalavra){
@@ -94,12 +62,12 @@ function listinha_letra_n_mais_letras(listinhaComPalavra){
     }
     return array
 }
-function mostrar_lista(array){
+export function mostrar_lista(array){
     for(let i = 1; i <= array.length;i++){
         console.log(`${i} - ${array[i-1]}`)
     }
 }
-function porcentagem(primeiroValor,segundoValor){
+export function porcentagem(primeiroValor,segundoValor){
     return ((segundoValor * 100)/primeiroValor)
 }
 function splitLetras(texto){
