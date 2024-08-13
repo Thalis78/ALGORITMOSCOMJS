aa2b98ccd99b99e1fb569d053354fe6911d5665a
Thalisson Moura <105744721+Thalis78@users.noreply.github.com>
2024-08-12 23:20:26 -0300

    Update desafio.js

--- WORDPLAY/desafio.js
+++ WORDPLAY/desafio.js
@@ -5,9 +5,12 @@ import { listinha_arquivo,listinha_com_vinte_letras,listinha_sem_letra_e,escolha
 
 
 function main(){
+    
     const lista_arquivo = listinha_arquivo();
     const lista_palavra_mais_vinte = listinha_com_vinte_letras(lista_arquivo);
     const lista_palavra_sem_E = listinha_sem_letra_e(lista_arquivo);
+    
     escolha_menu(lista_arquivo,lista_palavra_mais_vinte,lista_palavra_sem_E);
+    
 }
 main()
